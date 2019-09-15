1c672ea5aa242a264661d8cee9d88b7e55818aaa
Mohammed Saeb <mohammed.saeb98@gmail.com>
2019-09-15 17:49:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.querySelector ("#main");
+main.remove();
+
+let newHeader= document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
