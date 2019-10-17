101c6be9fda63e8b3d29fa0413d13e76c4bcefe4
Mohammed Ali <dody200039@gmail.com>
2019-10-17 16:09:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const v = "hi"
+
+document.querySelector("main#main").remove();
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "klay is the champion!";
+newHeader.className = "victory";
